92ef7fb39f4e5008b0cce720894486544ae4407c
Gerald Isabelle <info@geraldisabelle.me>
2016-12-21 00:20:04 +0000

    fix routing issue

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "index.html"
+        templateUrl : "main.html"
     })
     .when("/portfolio", {
         templateUrl : "portfolio.html"
